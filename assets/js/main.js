951eac9cfb8a85a6fb215fd57cd0ed3991baa1d5
KatKlee <katklee2022@gmail.com>
2022-07-28 17:40:52 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/* Function with various parameters. Output with console via string concatenations. Call the function with your input (arguments) */
+function myIntroduction(vorname, nachname, geburtsort, alter, wohnort) {
+    console.log('Mein Name ist ' + vorname + ' ' + nachname + '. Ich bin in ' + geburtsort + ' geboren. Ich lerne Coden bei SuperCode. Ich bin ' + alter + ' Jahre alt. Ich wohne in ' + wohnort + '.')
+}
+
+myIntroduction('Katrin', 'Kleemann', 'Deutschland', 35, 'Berlin')
